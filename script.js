4c504dbbf6d27383f988002bb5eced8600201f0f
owesleymauricio <135478971+owesleymauricio@users.noreply.github.com>
2023-06-06 19:47:07 -0300

    refeactor_css_alter

---
+++ script.js
@@ -0,0 +1,8 @@
+function on(){ 
+    document.querySelector('.lightBulb').classList.remove('white')
+     document.querySelector('.lightBulb').classList.add('yellow')
+ }
+ function off(){   
+     document.querySelector('.lightBulb').classList.remove('yellow')
+     document.querySelector('.lightBulb').classList.add('white')
+ }
